4d9c2abe5319dbfd70f2a1b86f32859a739fe833
alumno <alumno@Instituto.local>
2024-03-20 08:21:56 -0300

    archivojs

---
+++ index.js
@@ -0,0 +1,15 @@
+// comentario
+/* agnjopfangi
+fagjhoapfhg
+
+
+gfasñghsaop */
+
+alert('skibidi toilet');
+console.log('sdbghosibgoiabga');
+
+let nombrePais = 'Argentina'
+
+let suma = 80 + 20
+
+let multiplicación = suma * 8
